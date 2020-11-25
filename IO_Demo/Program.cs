4a91a2abea3119fa9c7282f3e8e2d41ce1cebdc9
Aynamik4 <violintailgut@gmail.com>
2020-11-25 12:11:25 +0100

    More IO

--- IO_Demo/Program.cs
+++ IO_Demo/Program.cs
@@ -5,9 +5,34 @@ namespace IO_Demo
 {
     class Program
     {
+        const string path = @"C:\Users\hakan\OneDrive\Desktop\Space distances.txt";
+
         static void Main(string[] args)
         {
-            //StreamReader sr = new StreamReader()
+            // StreamReader
+            string[] lines = new string[1000];
+            int index = -1;
+
+            using (StreamReader sr = new StreamReader(path))
+                while (!sr.EndOfStream)
+                    lines[++index] = sr.ReadLine();
+
+            for (int i = index; index - i <= index; i--)
+                Console.WriteLine($"{index - i + 1, 2}: {lines[index - i]}");
+
+            // StreamWriter
+            lines[++index] = "The End Again";
+
+            for (int i = index; i > -1; i--)
+                lines[i + 1] = lines[i];
+
+            index++;
+
+            lines[0] = "The Beginning";
+
+            using (StreamWriter sw = new StreamWriter(path))
+                for (int i = index; index - i <= index; i--)
+                    sw.WriteLine(lines[index - i]);
         }
     }
 }
