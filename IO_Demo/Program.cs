664310edb8ea690492728ba62fc20f97f311fdc0
Aynamik4 <violintailgut@gmail.com>
2020-11-25 10:28:56 +0100

    Added project IO_Demo

---
+++ IO_Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace IO_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader sr = new StreamReader()
+        }
+    }
+}
